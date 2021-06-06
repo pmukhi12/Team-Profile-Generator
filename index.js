598885f2f523910513bb6ce0020e4da9ee9429b4
Prav Mukhi <praveshmukhi@gmail.com>
2021-06-06 15:50:20 -0400

    set up the classes

---
+++ index.js
@@ -0,0 +1,65 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+
+// list of questions asked to the user
+const questions = require('./utils/questions');
+
+// htmlCardArray = [];
+
+// generate manager
+  // ask questions
+  // render new manager based on object props
+  // push to htmlCardArray
+
+// generate Engineer
+  // ask questions
+  // render new manager based on object props
+  // push to htmlCardArray
+  //  back to mainMenu
+
+
+// generate Inter
+  // ask questions
+  // render new manager based on object props
+
+
+//   const questions = {
+
+//     managerQuestions:
+//         [
+//             {
+//                 type: 'input',
+//                 name: 'name',
+//                 message: 'What is your name?',
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'id',
+//                 message: 'What is your id?',
+//             },
+//             {
+//                 type: 'input',
+//                 name: 'email',
+//                 message: 'What is your email address?',
+//             }
+//         ],
+//     internQuestions: [
+//         {
+//             type: 'input',
+//             name: 'name',
+//             message: 'What is your name?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'id',
+//             message: 'What is your id?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'email',
+//             message: 'What is your email address?',
+//         }
+//     ]
+// }
+
